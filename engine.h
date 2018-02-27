7dcc106afd3743225aec8d49bca281403aecae2e
bloodi11 <jds.dabrowa@gmail.com>
2018-02-27 16:50:28 +0100

    Add files via upload
    Pliki są puste, gotowy jest tylko preferowany wygląd graficznego interfejsu użytkownika. Jak na razie brak jakichkolwiek funkcjonalności.

---
+++ engine.h
@@ -0,0 +1,37 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+
+class Converter{
+private:
+
+public:
+
+};
+
+class VertexManager{
+private:
+
+public:
+
+};
+
+class Loader{
+private:
+
+public:
+
+};
+
+class Saver{
+  private:
+
+public:
+
+
+};
+
+
+
+
+
+#endif // ENGINE_H
